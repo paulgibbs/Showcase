538c0280afae203f59af2c355d7ea2cc56ed6cf2
Paul Gibbs <paul@byotos.com>
2013-04-28 13:16:08 +0100

    Rename/updated showcase-functions.phhp

--- templates/default/showcase-functions.php
+++ templates/default/showcase-functions.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 /** Theme Setup ***************************************************************/
 
-if ( !class_exists( 'BB_Default' ) ) :
+if ( ! class_exists( 'DPS_Default' ) ) :
 
 /**
  * Loads Showcase default theme functionality
@@ -28,21 +28,18 @@ if ( !class_exists( 'BB_Default' ) ) :
  * See @link DPS_Theme_Compat() for more.
  *
  * @since Showcase (1.0)
- *
  * @package Showcase
  * @subpackage DPS_Theme_Compat
  */
-class BB_Default extends DPS_Theme_Compat {
-
-	/** Functions *************************************************************/
+class DPS_Default extends DPS_Theme_Compat {
 
 	/**
 	 * The main Showcase (Default) Loader
 	 *
 	 * @since Showcase (1.0)
 	 *
-	 * @uses BB_Default::setup_globals()
-	 * @uses BB_Default::setup_actions()
+	 * @uses DPS_Default::setup_globals()
+	 * @uses DPS_Default::setup_actions()
 	 */
 	public function __construct() {
 		$this->setup_globals();
@@ -53,14 +50,11 @@ class BB_Default extends DPS_Theme_Compat {
 	 * Component global variables
 	 *
 	 * Note that this function is currently commented out in the constructor.
-	 * It will only be used if you copy this file into your current theme and
-	 * uncomment the line above.
+	 * It will only be used if you copy this file into your current theme and uncomment the line above.
 	 *
-	 * You'll want to customize the values in here, so they match whatever your
-	 * needs are.
+	 * You'll want to customize the values in here, so they match whatever your needs are.
 	 *
 	 * @since Showcase (1.0)
-	 * @access private
 	 */
 	private function setup_globals() {
 		$this->id      = 'default';
@@ -74,21 +68,14 @@ class BB_Default extends DPS_Theme_Compat {
 	 * Setup the theme hooks
 	 *
 	 * @since Showcase (1.0)
-	 * @access private
-	 *
-	 * @uses add_filter() To add various filters
-	 * @uses add_action() To add various actions
 	 */
 	private function setup_actions() {
 
 		/** Scripts ***********************************************************/
-
-		add_action( 'dps_enqueue_scripts',   array( $this, 'enqueue_styles'        ) ); // Enqueue theme CSS
-		add_action( 'dps_enqueue_scripts',   array( $this, 'enqueue_scripts'       ) ); // Enqueue theme JS
-
+		add_action( 'dps_enqueue_scripts', array( $this, 'enqueue_styles'  ) ); // Enqueue theme CSS
+		add_action( 'dps_enqueue_scripts', array( $this, 'enqueue_scripts' ) ); // Enqueue theme JS
 
 		/** Override **********************************************************/
-
 		do_action_ref_array( 'dps_theme_compat_actions', array( &$this ) );
 	}
 
@@ -96,8 +83,6 @@ class BB_Default extends DPS_Theme_Compat {
 	 * Load the theme CSS
 	 *
 	 * @since Showcase (1.0)
-	 *
-	 * @uses wp_enqueue_style() To enqueue the styles
 	 */
 	public function enqueue_styles() {
 
@@ -128,33 +113,9 @@ class BB_Default extends DPS_Theme_Compat {
 	 * Enqueue the required Javascript files
 	 *
 	 * @since Showcase (1.0)
-	 *
-	 * @uses dps_is_single_topic() To check if it's the topic page
-	 * @uses dps_is_single_user_edit() To check if it's the profile edit page
-	 * @uses wp_enqueue_script() To enqueue the scripts
 	 */
 	public function enqueue_scripts() {
-		$file = 'js/showcase.js';
-
-		// Check child theme
-		if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $file ) ) {
-			$location = trailingslashit( get_stylesheet_directory_uri() );
-			$handle   = 'dps-child-javascript';
-
-		// Check parent theme
-		} elseif ( file_exists( trailingslashit( get_template_directory() ) . $file ) ) {
-			$location = trailingslashit( get_template_directory_uri() );
-			$handle   = 'dps-parent-javascript';
-
-		// Showcase theme compatibility
-		} else {
-			$location = trailingslashit( dps_get_theme_compat_url() );
-			$handle   = 'dps-default-javascript';
-		}
-
-		// Enqueue the stylesheet
-		wp_enqueue_script( $handle, $location . $file, array(), $this->version, 'screen', true );
 	}
 }
-new BB_Default();
+new DPS_Default();
 endif;
